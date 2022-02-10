c27b290b5d72024422e8d4357ca7e42d3389093e
Mofbe <66583049+MofbeG@users.noreply.github.com>
2022-02-10 19:51:23 +0600

    Update js.js

--- scripts/js.js
+++ scripts/js.js
@@ -1,8 +1,6 @@
 let health = document.getElementById('health');
 let arm = document.getElementById('arm');
 let cash = document.getElementById('cash');
-let data = document.getElementById('data');
-let time = document.getElementById('time');
 
 cef.emit("game:hud:setComponentVisible", "interface", false);
 cef.emit("game:hud:setComponentVisible", "radar", true);
@@ -18,39 +16,3 @@ cef.on("game:data:playerStats", (hp, max_hp, arm, breath, wanted, weapon, ammo, 
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, delimiter || " ");
     }
 });
-
-function zero_first_format(value)
-{
-    if (value < 10)
-    {
-        value='0'+value;
-    }
-    return value;
-}
-
-//data
-function dates()
-{
-    var current_datetime = new Date();
-    var day = zero_first_format(current_datetime.getDate());
-    var month = zero_first_format(current_datetime.getMonth()+1);
-    var year = current_datetime.getFullYear();
-
-    data.innerHTML = day+"."+month+"."+year;
-}
-
-//time
-function times()
-{
-    var current_datetime = new Date();
-    var hours = zero_first_format(current_datetime.getHours());
-    var minutes = zero_first_format(current_datetime.getMinutes());
-    var seconds = zero_first_format(current_datetime.getSeconds());
-
-    time.innerHTML = hours+":"+minutes;
-}
-
-setInterval(function () {
-    dates();
-    times();
-}, 1000);
