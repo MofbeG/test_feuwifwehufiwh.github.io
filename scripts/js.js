b3de107628fbb4eb2de9b0111ad15a9f3543e860
Mofbe <66583049+MofbeG@users.noreply.github.com>
2022-02-10 19:18:02 +0600

    Update js.js

--- scripts/js.js
+++ scripts/js.js
@@ -16,3 +16,30 @@ cef.on("game:data:playerStats", (hp, max_hp, arm, breath, wanted, weapon, ammo, 
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, delimiter || " ");
     }
 });
+
+//data
+function dates()
+{
+    var current_datetime = new Date();
+    var day = zero_first_format(current_datetime.getDate());
+    var month = zero_first_format(current_datetime.getMonth()+1);
+    var year = current_datetime.getFullYear();
+
+    data.innerHTML = day+"."+month+"."+year;
+}
+
+//time
+function times()
+{
+    var current_datetime = new Date();
+    var hours = zero_first_format(current_datetime.getHours());
+    var minutes = zero_first_format(current_datetime.getMinutes());
+    var seconds = zero_first_format(current_datetime.getSeconds());
+
+    time.innerHTML = hours+":"+minutes;
+}
+
+setInterval(function () {
+    dates();
+    times();
+}, 1000);
